5d1b1fd3fec48411ae886d0cdaa9a1dc79f7a22d
Ryan Martin <ryan@vestahealthcare.com>
2022-12-13 15:38:45 -0500

    npx projen upgrade | update build config

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,26 @@
 import { ProjenProject } from '@rlmartin-projen/projen-project';
+import { NpmAccess } from 'projen/lib/javascript';
+
+const majorVersion = 0;
 const project = new ProjenProject({
   author: 'Ryan Martin',
-  authorAddress: 'ryan.martin@medly.com',
+  authorAddress: 'rlmartin@gmail.com',
   defaultReleaseBranch: 'main',
   devDeps: ['@rlmartin-projen/projen-project'],
-  name: 'cdktf-project',
+  name: '@rlmartin-projen/cdktf-project',
   projenrcTs: true,
   repositoryUrl: 'git@github.com:rlmartin-projen/cdktf-project.git',
+  releaseToNpm: true,
+  npmAccess: NpmAccess.PUBLIC,
+  majorVersion,
+  releaseBranches: {
+    dev: { prerelease: 'dev', npmDistTag: 'dev', majorVersion },
+  },
+  depsUpgradeOptions: {
+    workflowOptions: {
+      branches: ['main'],
+    },
+  },
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
