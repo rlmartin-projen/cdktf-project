58f5b2804a7e42d5a19b8e9b095654b775626e13
Ryan Martin <rlmartin@gmail.com>
2023-02-16 05:34:48 -0500

    fix: eslint failures

--- files/generated/src/index.ts
+++ files/generated/src/index.ts
@@ -5,5 +5,5 @@ import { pushStacks } from './stack';
 
 const app = new App();
 const stacks = pushStacks(app);
-Object.entries(stacks).forEach(([env, stack]) => pushBackend<TerraformStack>(stack, env as Environment<TerraformStack>))
+Object.entries(stacks).forEach(([env, stack]) => pushBackend<TerraformStack>(stack, env as Environment<TerraformStack>));
 app.synth();
