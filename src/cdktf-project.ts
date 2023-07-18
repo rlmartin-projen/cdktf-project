3b1addecabd0ff6eb168700e279036e8b3cdd4a5
Ryan Martin <rlmartin@gmail.com>
2023-07-18 10:22:34 -0400

    chore: update default NodeVersion

--- src/cdktf-project.ts
+++ src/cdktf-project.ts
@@ -253,7 +253,7 @@ export class CdktfProject extends typescript.TypeScriptProject {
   private embeddedPackageNames: Record<EmbeddedPackageType, string[]>;
 
   constructor(options: CdktfProjectOptions) {
-    const { nodeVersion = '16.3.0' } = options;
+    const { nodeVersion = '18.0.0' } = options;
     const nodeMajorVersion = nodeVersion.split('.')[0];
     const {
       artifactsFolder = 'dist',
