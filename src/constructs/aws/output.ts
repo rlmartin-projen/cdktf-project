51420cdd2608eb848fbb30a7199b713d6bbebcba
Ryan Martin <rlmartin@gmail.com>
2025-06-25 13:47:40 -0400

    feat: add Output construct (#57)
    * feat: add Output construct
    
    * chore: self mutation
    
    Signed-off-by: github-actions <github-actions@github.com>
    
    ---------
    
    Signed-off-by: github-actions <github-actions@github.com>
    Co-authored-by: github-actions <github-actions@github.com>

---
+++ src/constructs/aws/output.ts
@@ -0,0 +1,39 @@
+import { SsmParameter } from '@cdktf/provider-aws/lib/ssm-parameter';
+import { Fn, TerraformOutput } from 'cdktf';
+import { Construct } from 'constructs';
+import { TaggedConstruct, TaggedConstructConfig } from './taggedConstruct';
+
+type ParameterType = 'String' | 'StringList' | 'SecureString';
+
+export interface OutputConfig extends TaggedConstructConfig {
+  readonly description?: string;
+  readonly namespace: string;
+  readonly value: string | string[];
+  readonly sensitive?: boolean;
+}
+
+/**
+ * A construct that stores outputs in AWS ParameterStore, for use in other stacks.
+ */
+export class Output extends TaggedConstruct {
+  constructor(scope: Construct, id: string, config: OutputConfig) {
+    super(scope, id, config);
+    const { description, namespace, sensitive = false, tags, value } = config;
+    const paramType: ParameterType = sensitive ? 'SecureString' : Array.isArray(value) ? 'StringList' : 'String';
+
+    new TerraformOutput(this, id, {
+      value,
+      description,
+      sensitive,
+      staticId: true,
+    });
+
+    new SsmParameter(this, 'parameter', {
+      name: `/${namespace}/${id}`,
+      value: Array.isArray(value) ? Fn.join(',', value) : value,
+      description,
+      type: paramType,
+      tags,
+    });
+  }
+}
