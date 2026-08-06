b30cb11fa85f565363cdc5c69078704c2ad5fc8c
Ryan Martin <rlmartin@gmail.com>
2026-08-06 09:30:19 -0400

    fix: unify release in one triggering file, for NPM Truster Publisher

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,6 +1,9 @@
 import { ProjenProject } from '@rlmartin-projen/projen-project';
+import { GithubWorkflow } from 'projen/lib/github';
 import { NpmAccess } from 'projen/lib/javascript';
+import { ReleaseTrigger } from 'projen/lib/release/release-trigger';
 import { sharedDeps } from './src/cdktf-project';
+import { JobPermission } from 'projen/lib/github/workflows-model';
 
 const majorVersion = 8;
 const project = new ProjenProject({
@@ -25,6 +28,7 @@ const project = new ProjenProject({
   releaseBranches: {
     dev: { prerelease: 'dev', npmDistTag: 'dev', majorVersion },
   },
+  releaseTrigger: ReleaseTrigger.workflowDispatch(),
   depsUpgradeOptions: {
     workflowOptions: {
       branches: ['main'],
@@ -34,5 +38,27 @@ const project = new ProjenProject({
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // packageName: undefined,  /* The "name" in package.json. */
+});
+const releaseAll = new GithubWorkflow(project.github!, 'release-all', {});
+releaseAll.on({
+  push: {
+    branches: ['main', 'dev'],
+  },
+});
+releaseAll.addJob('publish-prod', {
+  if: "github.ref_name == 'main'",
+  uses: './.github/workflows/release.yml',
+  permissions: {
+    idToken: JobPermission.WRITE,
+    contents: JobPermission.READ,
+  },
+});
+releaseAll.addJob('publish-beta', {
+  if: "github.ref_name == 'dev'",
+  uses: './.github/workflows/release-dev.yml',
+  permissions: {
+    idToken: JobPermission.WRITE,
+    contents: JobPermission.WRITE,
+  },
 });
 project.synth();
